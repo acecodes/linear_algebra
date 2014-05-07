eb1b64176682cfc58847cb1ec692fd70c4ea0ebd
Ace <real.deal.ace@gmail.com>
2014-05-06 18:05:20 -0700

    Added a zero vector procedure and a very basic vector class

--- vectors.py
+++ vectors.py
@@ -5,3 +5,17 @@ def add_vectors(vec_list1, vec_list2):
 # Multiply a set of vectors by a scalar
 def scalar_mul_vectors(scalar, vectorlist):
 	return [x * scalar for x in vectorlist]
+
+# A class representation of a vector
+# f is the function and must be represented by a dictionary
+# D is the domain of the function and must be a set
+class Vec:
+	def __init__(self, labels, function):
+		self.D = labels
+		self.f = function
+
+# Assigns zero vectors to each element in a set
+def zero_vec(D):
+	return Vec(D, {d:0 for d in D})
+
+TestVector = Vec({'A', 'B', 'C'}, {'A':1.})
